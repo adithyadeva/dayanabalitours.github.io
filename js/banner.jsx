b9e67f9ae96998e92f7a8c521684ccf893a53e4b
adithyadeva <adityadeva515@gmail.com>
2022-08-26 20:03:45 +0800

    first commit

---
+++ js/banner.jsx
@@ -0,0 +1,11 @@
+const menu = document.querySelector('.menu-toggle input');
+const nav = document.querySelector('nav ul');
+
+
+menu.addEventListener('click', function(){
+    nav.classList.toggle('slide');
+})
+
+
+
+
